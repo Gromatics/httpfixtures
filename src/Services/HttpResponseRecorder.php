5b99e57671a2436e2d34899f3f403b1f44df21dc
Guido <G.rus@de-rus.nl>
2025-05-09 09:58:29 +0200

    Better console messages and usage example when generating fixtures with recordedToHttpFixture

--- src/Services/HttpResponseRecorder.php
+++ src/Services/HttpResponseRecorder.php
@@ -9,7 +9,8 @@ class HttpResponseRecorder
 {
     public static function recordedToHttpFixture(?string $newFixtureName = null): void
     {
-        Http::recorded(function ($request, $response) use ($newFixtureName) {
+        $fixtureData = [];
+        Http::recorded(function ($request, $response) use (&$fixtureData, $newFixtureName) {
             if (is_array($response->json())) {
 
                 if (!$newFixtureName) {
@@ -22,12 +23,20 @@ class HttpResponseRecorder
                 }
 
                 FileModificationService::copyExampleHttpFixture($newFixtureName, json_encode($response->json()), true);
-
-                fwrite(STDOUT, "{$newFixtureName} created in /tests/Fixtures");
-
+                fwrite(STDOUT, "\n\033[42m\033[30mFixture created for url: " . Str::limit($request->url(), 50) . " >  /tests/Fixtures/{$newFixtureName}\033[0m\n");
+                $fixtureData[] = ['url' => $request->url(), 'fixture' => $newFixtureName];
             } else {
-                fwrite(STDOUT, "\033[31m" . 'Could not create HTTP Fixture. Request URL: ' . $request->url() . " returns invalid JSON. \033[0m\n");
+                fwrite(STDOUT, "\033[31m" . 'Could not create HTTP Fixture. Request URL: ' . Str::limit($request->url(), 50) . " returns invalid JSON. \033[0m\n");
             }
         });
+
+        //Show Example in console
+        if (!empty($fixtureData)) {
+            fwrite(STDOUT, "\033[94m" . "Http::fake([" . "\033[0m\n");
+            foreach ($fixtureData as $fData) {
+                fwrite(STDOUT, "\033[94m " . "'" . strtok($fData['url'], '?') . "*' => Http::response((new \Tests\Fixtures\\" . $fData['fixture'] . "())->toJson(),  200)" . (next($fixtureData) ? ',' : '') . "\033[0m\n");
+            }
+            fwrite(STDOUT, "\033[94m" . "]);" . "\033[0m\n");
+        }
     }
 }
